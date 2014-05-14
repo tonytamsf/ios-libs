4e5715e5869c86f2aee1ee466822724c8596bdee
Tony Tam <tonytam@yahoo-inc.com>
2014-05-14 12:07:43 -0700

    - refactoring to get ready to verify answer

---
+++ model/cards/PlayingCardDeckNoFace.h
@@ -0,0 +1,15 @@
+//
+//  PlayingCardDeckNoFace.h
+//  Provides a deck of cards where the face cards are replaced by
+//  10.  This can be used as a simpler deck of cards for the game
+//  of 24
+//
+//  Created by Tony Tam on 5/13/14.
+//  Copyright (c) 2014 Yama Llama. All rights reserved.
+//
+
+#import "PlayingCard.h"
+
+@interface PlayingCardDeckNoFace : PlayingCard
+
+@end
---
+++ model/cards/PlayingCardDeckNoFace.m
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeckNoFace.m
+//  Get To 24
+//
+//  Created by Tony Tam on 5/13/14.
+//  Copyright (c) 2014 Yama Llama. All rights reserved.
+//
+
+#import "PlayingCardDeckNoFace.h"
+
+@implementation PlayingCardDeckNoFace
+
+@end
